5b5e89c76edb9584069791187d67e1804dc7ed0c
Marco Biedermann <marco.biedermann@gmx.de>
2017-08-16 00:49:01 +0200

    install PHP

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,9 @@ MYSQL_NAME="typo3"
 
 sudo -s
 
+apt-get -y install software-properties-common
+add-apt-repository ppa:ondrej/php
+
 # Update OS
 apt-get -y update
 apt-get -y upgrade
@@ -27,6 +30,19 @@ apt-get -y install mysql-server
 
 # Create Database
 mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASS -e "CREATE DATABASE $MYSQL_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+
+# Install PHP
+apt-get -y install libapache2-mod-php7.0
+apt-get -y install php7.0
+apt-get -y install php7.0-gd
+apt-get -y install php7.0-mysql
+apt-get -y install php7.0-soap
+apt-get -y install php7.0-xml
+apt-get -y install php7.0-zip
+
+# Set Options in php.ini
+sed -i "s/^max_execution_time.*/max_execution_time = 240/" /etc/php/7.0/apache2/php.ini
+sed -i "s/^; max_input_vars.*/max_input_vars = 1500/" /etc/php/7.0/apache2/php.ini
 
 # Enable Apache Modules
 a2enmod rewrite
